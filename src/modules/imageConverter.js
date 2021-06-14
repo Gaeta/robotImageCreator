dc897f17c3d8a7acfff02944e2c65cd580c6869a
D Gaeta <thespider84@gmail.com>
2021-06-13 21:26:11 -0400

    Code Cleanup

--- src/modules/imageConverter.js
+++ src/modules/imageConverter.js
@@ -4,11 +4,10 @@ const sharp = require('sharp');
 
 var imageConverter = async (image /* Requires to be a buffer */) => {
   await new Promise(async (resolve, reject) => {
-    await sharp(image).greyscale().toBuffer().then(async data => {
+    await sharp(image).greyscale().toBuffer().then(async data => { // Changes images to grayscale
       image = new ImageParser(data);
       await image.parse(async err => {
         if (err) { return console.error(err); }
-        // console.log(image.pixels())
         await image.resize(Config.paperSize.height, Config.paperSize.width, (err, data) => { //Width & Height are fliped to fit on paper correctly :D
           if (err) { return console.error(err); }
           image = data
@@ -23,14 +22,14 @@ var imageConverter = async (image /* Requires to be a buffer */) => {
     let pixels = [];
 
     let size = {
-        height: image.height()
-      , width: image.width()
+      height: image.height(), 
+      width: image.width()
     };
 
     for (let y = 0; y < size.height; ++y) {
-        for (let x = 0; x < size.width; ++x) {
-            pixels.push({rgba: image.getPixel(x, y), cords: {x: x, y: y}});
-        }
+      for (let x = 0; x < size.width; ++x) {
+        pixels.push({rgba: image.getPixel(x, y), cords: {x: x, y: y}});
+      }
     }
 
     if (filterTransparentPixels) return pixels.filter(g => g.rgba.a <= 0)
